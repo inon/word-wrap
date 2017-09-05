d2a93ca239a7c67788d7fca07401f1c604dc71be
Inon Baguio <inon.baguio@yahoo.com>
2017-09-05 15:06:04 +0800

    Created word wrap function

---
+++ src/Wrapper/WordWrap.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Inon\Wrapper;
+
+/**
+ * The WordWrap class.
+ *
+ * @package Inon\WordWrap
+ * @author  Inon Baguio <inon.baguio@yahoo.com>
+ */
+class WordWrap
+{
+
+    #----------------------------------------------------------------------------------------------
+    # Properties
+    #----------------------------------------------------------------------------------------------
+
+    /**
+     * @var string
+     */
+    private $string = '';
+
+    /**
+     * @var int
+     */
+    private $length = 0;
+
+    /**
+     * @var array
+     */
+    private $wordsArray = [];
+
+    /**
+     * WordWrap constructor.
+     *
+     * @param string $string
+     * @param int $length
+     */
+    public function __construct(string $string, int $length)
+    {
+        $this->string = $string;
+        $this->length = $length;
+
+        $this->wordsArray = explode(' ', $this->string);
+    }
+
+    #----------------------------------------------------------------------------------------------
+    # Class methods
+    #----------------------------------------------------------------------------------------------
+
+    /**
+     * @return string
+     */
+    public function handle() : string
+    {
+        $newWord = $temp = '';
+
+        $arrayLength = count($this->wordsArray);
+
+        for ($i=0; $i < $arrayLength; $i++) {
+            if ($i === $arrayLength - 1) {
+                $temp = $this->assembleString($this->wordsArray[$i]);
+            } else {
+                if (strlen($this->wordsArray[$i] . $this->wordsArray[$i + 1]) <= $this->length) {
+                    $temp = $this->assembleString([$this->wordsArray[$i], $this->wordsArray[$i + 1]]);
+                    $i++;
+                } else {
+                    $temp = $this->assembleString($this->wordsArray[$i]);
+                }
+            }
+
+            $newWord .= $temp;
+
+            $temp = '';
+        }
+
+        return $newWord;
+    }
+
+    /**
+     * @param array|string $word
+     *
+     * @return string
+     */
+    private function assembleString($word) : string
+    {
+        if (is_array($word)) {
+            return implode('', $word) . "\n";
+        }
+
+        return $word . "\n";
+    }
+}
---
+++ test/WordWrapTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Inon\WordWrap\Test;
+
+/**
+ * The WordWrapTest class.
+ *
+ * @package Inon\WordWrap\Test
+ * @author  Inon Baguio <inon.baguio@yahoo.com>
+ */
+class WordWrapTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testSample()
+    {
+        $this->assertTrue(true);
+    }
+}
